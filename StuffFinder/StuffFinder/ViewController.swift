3bc5cd821e1b36ca1cdf25485c0f7c641308cf66
Jayjay <jayjayvenegas@gmail.com>
2020-03-23 10:03:20 -0700

    new

---
+++ StuffFinder/StuffFinder/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  StuffFinder
+//
+//  Created by jayjay venegas on 3/23/20.
+//  Copyright © 2020 jayjay venegas. All rights reserved.
+//
+import MapKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
